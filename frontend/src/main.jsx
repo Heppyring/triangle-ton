20e11e4bfda00bbeac2e7316838df4ecf9228bd0
Heppyring <232@gmail.com>
2026-04-01 23:38:33 +0300

    TON connect working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl="https://chronically-brightish-freida.ngrok-free.dev/tonconnect-manifest.json">
     <App />
   </TonConnectUIProvider>
 )
