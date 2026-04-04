57f72cbdd9ed7b4d02b0f57babbd4fb927f42aa5
Heppyring <232@gmail.com>
2026-04-04 11:23:18 +0300

    fix: absolute manifest url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <TonConnectUIProvider manifestUrl="https://chronically-brightish-freida.ngrok-free.dev/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl="manifestUrl="https://triangle-ton.vercel.app/tonconnect-manifest.json"">
     <App />
   </TonConnectUIProvider>
 )
